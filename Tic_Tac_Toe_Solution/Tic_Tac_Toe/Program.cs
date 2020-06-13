a522dfffb56a05007288bde41485b72d5da87fc3
Aleksandr <grigxia86@gmail.com>
2020-06-13 21:23:46 +0300

    C# Essential course exercise

---
+++ Tic_Tac_Toe_Solution/Tic_Tac_Toe/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********Welcome to the game Tic-Tac-Toe!**********");
+            TicTacToeBoard tttBoard = new TicTacToeBoard();
+
+            int mode = tttBoard.ModeOfGame();
+
+            switch (mode)
+            {
+                case 1:
+                    ModePlayer1VsPlayer2(tttBoard);
+                    break;
+
+                case 2:
+                    Console.WriteLine("\nDo you want to write the results of the game to a file (y - Yes): ");
+                    string yesOrNo = Console.ReadLine();
+
+                    // регистрируем событие
+                    if (yesOrNo.ToLower().Equals("y"))
+                    {
+                        tttBoard.WriteToFile += ResultToFile;
+                    }
+                    ModePlayer1VsComp(tttBoard);
+                    break;
+
+                default:
+                    Console.WriteLine("Error of choice!"); 
+                    break;
+            }
+        }
+
+        
+
+        static void ModePlayer1VsPlayer2(TicTacToeBoard board)
+        {
+          
+            do
+            {
+                Console.WriteLine("Determine who goes first: press your character or press \'r\' and it will be chosen randomly:");
+
+                char prime;
+                while (!char.TryParse(Console.ReadLine(), out prime) && !char.IsLetter(prime))
+                {
+                    Console.WriteLine("Invalid character! Follow the instructions.");
+                }
+
+                if (prime == board.Chip1)
+                {
+                    prime = board.Chip1;
+                }
+                else if(prime == board.Chip2)
+                {
+                    prime = board.Chip2;
+                }
+                else if (prime == 'r')
+                {
+                    Random rand = new Random();
+                    int r = rand.Next(1, 1000);
+                    if (r % 2 == 0)  prime = board.Chip1; 
+                    else  prime = board.Chip2; 
+                }
+                else
+                {
+                    Console.WriteLine($"Such a chip does not exist! Default state, first: {board.Chip1}");
+                    prime = board.Chip1;
+
+                }
+
+                Console.WriteLine("~~~~~~~~~~~The game started~~~~~~~~~~~");
+                while (!board.IsFullBoard)
+                {
+                    board.showBoard();
+                    Console.WriteLine($"Puts the chip player \'{prime}\':");
+                    Console.WriteLine($"Select the position (0 - 8) where to insert the chip: ");
+                    int pos;
+                    while (!int.TryParse(Console.ReadLine(), out pos))
+                    {
+                        Console.WriteLine("This is not a number!. Follow the instructions!");
+                    }
+                    board.changeBoard(prime, pos);
+                    if (board.IsWinner(prime))
+                    {
+                        board.showBoard();
+                        break;
+                    }
+
+                    if (prime == board.Chip1) { prime = board.Chip2; }
+                    else
+                    { prime = board.Chip1; }
+                }
+
+                if (board.IsFullBoard)
+                {
+                    Console.WriteLine("The board is full. No changes");
+                    board.showBoard();
+                }
+
+            } while (QuestionOnRepeat(board));
+
+            Console.WriteLine("Goodbye!!!");
+            Console.ReadLine();
+        }
+
+        static void ModePlayer1VsComp(TicTacToeBoard board)
+        {
+            
+            do
+            {
+
+                Console.WriteLine("\nFirst to play Player 1");
+                char prime = board.Chip1;
+                Console.WriteLine("~~~~~~~~~~~The game started~~~~~~~~~~~");
+                while (!board.IsFullBoard)
+                {
+                    board.showBoard();
+                    Console.WriteLine($"Puts the chip player \'{prime}\':");
+                    Console.WriteLine($"Select the position (0 - 8) where to insert the chip: ");
+                    int pos;
+                    while (!int.TryParse(Console.ReadLine(), out pos))
+                    {
+                        Console.WriteLine("This is not a number!. Follow the instructions!");
+                    }
+                    board.changeBoard(prime, pos);
+                    board.showBoard();
+                    if (board.IsWinner(prime))
+                    {
+                        board.showBoard();
+                        break;
+                    }
+
+                    if (board.IsFullBoard)
+                    {
+                        Console.WriteLine("The board is full.");
+                        break;
+                    }
+
+                    Console.WriteLine("The computer puts its chip");
+                    int compM = board.CompMove();
+                    Console.WriteLine($"CompMove = {compM}");
+                    board.changeBoard(board.ChipComp, compM);
+                    if (board.IsWinner(board.ChipComp))
+                    {
+                        board.showBoard();
+                        break;
+                    }
+
+                    prime = board.Chip1;
+
+                    if (board.IsFullBoard)
+                    {
+                        Console.WriteLine("The board is full. No changes");
+                        board.showBoard();
+                    }
+
+                }
+
+                
+
+            } while (QuestionOnRepeat(board));
+        }
+
+        static bool QuestionOnRepeat(TicTacToeBoard board)
+        {
+            string answer;
+            Console.WriteLine("Want to play one more time?(y/n)");
+            answer = Console.ReadLine();
+
+            if (answer.ToLower().Equals("y"))
+            {
+                board.ResetBoard();
+                board.ClearListPositionsAndCharArray();
+                board.IsFullBoard = false;
+                return true;
+            }
+            else
+                Console.WriteLine("This is NO!");
+                return false;
+
+        }
+
+        // Метод для обработки события записи на диск результатов
+        public static void ResultToFile(object sender, TicTacToeEventArgs ev)
+        {
+            Console.WriteLine(ev.Massage);
+            TicTacToeBoard newBoard = sender as TicTacToeBoard;
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            string dirName = drives[0].Name;
+
+            if(drives[0].AvailableFreeSpace > 10000  && drives[0].IsReady)
+            {
+                var dir = Directory.CreateDirectory(dirName + "ResultOfTic-Tac-Toe");
+                string path = dir.ToString() + "\\result.txt";
+
+                string result = $"Побед компьютера: {ev.CountComp}\nПобед игрока: {ev.CountPlayer}";
+
+                File.WriteAllText(path, result);
+            }
+
+        }
+    }
+}
---
+++ Tic_Tac_Toe_Solution/Tic_Tac_Toe/TicTacToeBoard.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Tic_Tac_Toe
+{
+
+    public delegate void MyAction(object sender, TicTacToeEventArgs e);
+    
+    public class TicTacToeEventArgs 
+    {
+        public string Massage { get; }
+        public int CountComp { get; }
+        public int CountPlayer { get; }
+
+        public TicTacToeEventArgs(string massage, int countComp, int countPlayer)
+        {
+            Massage = massage;
+            CountComp = countComp;
+            CountPlayer = countPlayer;
+        }
+
+    }
+    public class TicTacToeBoard
+    {
+
+        // Событие определяющее запись результатов игры в файл
+        public event MyAction WriteToFile;
+
+        private string board = @"
+              |     |     
+           0  |  1  |  2  
+        ------------------  
+              |     |     
+           3  |  4  |  5  
+        ------------------
+              |     |     
+           6  |  7  |  8  
+              |     |      ";
+
+        private List<int> listPositions = new List<int>();
+        private readonly Dictionary<int, int> map = null;
+        private int _countWinComp = 0;
+        private int _countWinPlayer = 0;
+        
+        // Свойство, определяющее заполненность доски фишками-символами
+        public bool IsFullBoard { get; set; } = false;
+
+        private char _chip1;
+        private char _chip2;
+        public char Chip1 { get; private set; }
+        public char Chip2 { get; private set; }
+        public char ChipComp { get; private set; } = 'y';
+
+
+
+        // Конструтор выполняющий создание карты-позиционирования
+        public TicTacToeBoard()
+        {
+            this.map = new Dictionary<int, int>()
+            {
+                { 0, this.board.IndexOf('0') },
+                { 1, this.board.IndexOf('1') },
+                { 2, this.board.IndexOf('2') },
+                { 3, this.board.IndexOf('3') },
+                { 4, this.board.IndexOf('4') },
+                { 5, this.board.IndexOf('5') },
+                { 6, this.board.IndexOf('6') },
+                { 7, this.board.IndexOf('7') },
+                { 8, this.board.IndexOf('8') }
+
+            };
+        }
+
+        
+        public void showBoard()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(board);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("========================================");
+            /*WriteToFile?.Invoke(this, new TicTacToeEventArgs("Recording completed!", _countWinComp, _countWinPlayer));*/
+        }
+
+        // Метод выбора режима игры(2 игрока или игрок против компьютера)
+        public int ModeOfGame()
+        {
+            Console.WriteLine("Select game mode:\n" +
+                              "1. Two players\n" +
+                              "2. Single player\n" +
+                              "3. Exit");
+            int mode;
+
+
+            while (!int.TryParse(Console.ReadLine(), out mode))
+            {
+                Console.WriteLine("Invalid mode! Press 1 or 2.");
+            }
+
+            while (mode != 1 && mode != 2 && mode != 3)
+            {
+                Console.WriteLine("No such game mode!");
+                while (!int.TryParse(Console.ReadLine(), out mode))
+                {
+                    Console.WriteLine("Invalid mode! Press 1 or 2.");
+                }
+            }
+
+
+            switch (mode)
+            {
+                case 1:
+                    Console.WriteLine("Your choice: Two players");
+                    ChoiceСhip("Player 1", ref _chip1);
+                    ChoiceСhip("Player 2", ref _chip2);
+                    Chip1 = _chip1;
+                    Chip2 = _chip2;
+                    if (Chip1 == Chip2) 
+                    {
+                        Chip2 = 'z';
+                        Console.WriteLine($"You cannot play with the same chips. Player 2 will play: {Chip2}\n");
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine($"Computer plays chip: {ChipComp}");
+                    ChoiceСhip("Player 1", ref _chip1);
+                    Chip1 = _chip1;
+                    break;
+
+                case 3:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+
+            }
+            
+            return mode;
+        }
+
+        private void ChoiceСhip(string numbOfPlayers, ref char сhip)
+        {
+            Console.WriteLine($"{numbOfPlayers}, please select a chip to play (one character):");
+
+            char choice;
+            while (!char.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid character! Follow the instructions.");
+            }
+            while (char.IsDigit(choice) || char.IsWhiteSpace(choice) || choice == ChipComp)
+            {
+                Console.WriteLine("Invalid character! Follow the instructions.");
+                while (!char.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid character! Follow the instructions.");
+                }
+            }
+            сhip = choice;
+        }
+
+        // Метод, следящий за правильной логикой изменения положений на доске
+        public void changeBoard(char symbol, int position)
+        {
+
+            
+
+            if (!IsFullBoard)
+            {
+                while (position < 0 || position > 8 || this.listPositions.Contains(position))
+            {
+                Console.WriteLine("Wrong position! Try one more time!");
+                while(!int.TryParse(Console.ReadLine(), out position))
+                {
+                    Console.WriteLine("This is not a number! Follow the instructions!");
+                }
+            }
+
+            
+                this.listPositions.Add(position);
+                
+                if (listPositions.Count == 9) { IsFullBoard = true; }
+
+                this.board = RebuildBoard(symbol, position);
+            }
+            else
+            {
+                Console.WriteLine("The board is full. No changes");
+            }
+            
+           
+
+        }
+
+        // Метод, перестраивающий доску-строку с учётом установленной фишки-символа
+        private string RebuildBoard(char symbol, int position)
+        {
+            string beginBoard = this.board.Substring(0, map[position]);
+            string symbolInBoard = symbol.ToString();
+            string endBoard = this.board.Substring(map[position] + 1);
+
+            string newBoard = beginBoard + symbolInBoard + endBoard;
+            return newBoard;
+        }
+
+        // Метод, определяющий победителя по фишке-символу игрока
+        public bool IsWinner(char symbol)
+        {
+            // Определим выигрышные комбинации
+            int[,] combWin = new int[8, 3]  
+            {
+                { 0, 1, 2}, { 3, 4, 5}, { 6, 7, 8}, { 0, 3, 6},
+                { 1, 4, 7}, { 2, 5, 8}, { 0, 4, 8}, { 2, 4, 6}
+            };
+
+            int first = 0, second = 0, third = 0;
+            for (int i = 0; i < combWin.GetLength(0); i++)
+            {
+                int j = 0;
+              
+                 first = combWin[i, j++];
+                 second = combWin[i, j++];
+                 third = combWin[i, j++];
+                
+                if (board[map[first]] == board[map[second]] && board[map[second]] == board[map[third]]
+                    && board[map[third]] == symbol)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\n*************Congratulation!!!**************");
+
+                    Console.WriteLine($"Winner is: {symbol}!\n");
+                    
+                    
+                        if (symbol == ChipComp)
+                        {
+                            _countWinComp++;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("This is a computer victory! A shame!");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                        else
+                        {
+                            _countWinPlayer++;
+                        }
+
+
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    WriteToFile?.Invoke(this, new TicTacToeEventArgs("Recording completed!", _countWinComp, _countWinPlayer));  //  если событие для обработки добавлено (т.е. не равно null) то обработаем его
+                    Console.WriteLine("**********************************************");
+
+                    
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        // Метод, определяющий предвыирышную ситуацию и возвращающий позицию, которая
+        // приведёт к победе, либо -1, если таковой нет (метод для поддержания логики игры компьютера)
+        private int AlmostWinnner()
+        {
+            int[,] combWin = new int[8, 3]
+            {
+                { 0, 1, 2}, { 3, 4, 5}, { 6, 7, 8}, { 0, 3, 6},
+                { 1, 4, 7}, { 2, 5, 8}, { 0, 4, 8}, { 2, 4, 6}
+            };
+
+            int first = 0, second = 0, third = 0;
+            for (int i = 0; i < combWin.GetLength(0); i++)
+            {
+                int j = 0;
+
+                first = combWin[i, j++];
+                second = combWin[i, j++];
+                third = combWin[i, j++];
+
+                if (board[map[first]] == board[map[second]] && board[map[third]] != Chip1 && board[map[third]] != ChipComp) return third;
+                else if (board[map[second]] == board[map[third]] && board[map[first]] != Chip1 && board[map[first]] != ChipComp) return first;
+                else if (board[map[first]] == board[map[third]] && board[map[second]] != Chip1 && board[map[second]] != ChipComp) return second;
+                else continue;
+
+            }
+            return -1;
+        }
+
+        // Метод Логики игры компьютера
+        public int CompMove()
+        {
+           /* int posReturn = AlmostWinnner();*/
+            // Если есть предвыигрышная ситуация, то возвращаем её
+            if(AlmostWinnner() != -1)
+            {
+                return AlmostWinnner();
+            }
+            // Если метод вернул -1, то ищем рандомную позицию, путём создания списка
+            // из всех позиций. Затем вычитание тех позиций, что уже есть в глобальном
+            // списке состояния. И у получившегося списка по индексу, выбранному рандомно
+            // возвращаем позицию.
+            else
+            {
+                int rand = new Random().Next(0, 9);
+                while(this.listPositions.Contains(rand))
+                {
+                    rand = new Random().Next(0, 9);
+                }
+                return rand;
+                /*var allPos = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+                var result = allPos.Except(listPositions).ToList();
+
+                int index = new Random().Next(0, result.Count);
+                
+                return result[index];*/
+                                       
+            }
+        }
+
+        // Метод, возвращающий доску в исходное состояние
+        public void ResetBoard()
+        {
+            string primaryBoard = @"
+              |     |     
+           0  |  1  |  2  
+        ------------------  
+              |     |     
+           3  |  4  |  5  
+        ------------------
+              |     |     
+           6  |  7  |  8  
+              |     |      ";
+
+            this.board = primaryBoard;
+        }
+
+        public void ClearListPositionsAndCharArray()
+        {
+            this.listPositions.Clear();       
+        }
+
+
+    }
+
+}
